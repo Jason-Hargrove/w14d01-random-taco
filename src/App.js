33b6b230af6bc197396c3a7484dc4ae5a405a83a
Jason Hargrove <rijaha@gmail.com>
2021-07-26 08:04:09 -0500

    It works.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import TacoInfo from './TacoInfo';
 
 function App(props) {
 
-  const [name, updateName] = useState("Jason");
+  const [name] = useState("Want a Random Taco?");
 
   const[query, updateQuery] = useState({
     searchURL: 'http://taco-randomizer.herokuapp.com/random/?full-tack=true',
   });
 
   const [taco, updateTaco] = useState({});
+
+
   useEffect(() => {
       query.searchURL.length > 0 &&
         (async () => {
           try {
             const response = await fetch(query.searchURL);
             const data = await response.json();
-            updateTaco( ['fart'] );
+            updateTaco({ ...data });
             updateQuery({ ...query, searchURL:'' });
           } catch (e) {
               console.error(e);
@@ -25,31 +27,18 @@ function App(props) {
         })();
   }, [query]);
 
-  const handleChange = event => {
-    updateQuery( ...query, ...{ [event.target.id]: event.target.value});
-  };
-
-  const handleSubmit = async event => {
-    event.preventDefault();
-    updateQuery({
-      ...query,
-    });
-  };
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
     return (
     <div className="page-wrapper">
-      <h2>Hypatia App</h2>
-      <h3>{name}</h3>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title"> Title </label>
-        <input
-          id="title"
-          type="text"
-          value={query.slug}
-          onChange={handleChange}
-        />
-        <input type="submit" value="Find Random Taco" />
-      </form>
+      <h1>{name}</h1>
+      <div>
+        <button onClick={refreshPage}>
+          Click for a Different Taco
+        </button>
+      </div>
       <main className="page">
         {Object.keys(taco).length ? <TacoInfo taco={taco} /> : ''}
       </main>
--- src/TacoInfo.js
+++ src/TacoInfo.js
@@ -1,11 +1,30 @@
 const TacoInfo = (props) => {
   return (
     <div className="column">
-      <h1>Mixin: {props.taco.mixin}</h1>
-      <h2>Base Layer: {props.taco.base_layer}</h2>
-      <h3>Seasoning: {props.taco.seasoning}</h3>
-      <h4>Shell: {props.taco.shell}</h4>
-      <h4>Condiment: {props.taco.condiment}</h4>
+      <h2>Mixin: {props.taco.mixin.slug}</h2>
+      <h3><a href={props.taco.mixin.url}>Link to the Recipe</a></h3>
+      <h3>Name: {props.taco.mixin.name}</h3>
+      <h3>Recipe: {props.taco.mixin.recipe}</h3>
+
+      <h2>Base Layer: {props.taco.base_layer.slug}</h2>
+      <h3><a href={props.taco.base_layer.url}>Link to the Recipe</a></h3>
+      <h3>Name: {props.taco.base_layer.name}</h3>
+      <h3>Recipe: {props.taco.base_layer.recipe}</h3>
+
+      <h2>Seasoning: {props.taco.seasoning.slug}</h2>
+      <h3><a href={props.taco.seasoning.url}>Link to the Recipe</a></h3>
+      <h3>Name: {props.taco.seasoning.name}</h3>
+      <h3>Recipe: {props.taco.seasoning.recipe}</h3>
+
+      <h2>Shell: {props.taco.shell.slug}</h2>
+      <h3><a href={props.taco.shell.url}>Link to the Recipe</a></h3>
+      <h3>Name: {props.taco.shell.name}</h3>
+      <h3>Recipe: {props.taco.shell.recipe}</h3>
+
+      <h2>Condiment: {props.taco.condiment.slug}</h2>
+      <h3><a href={props.taco.condiment.url}>Link to the Recipe</a></h3>
+      <h3>Name: {props.taco.condiment.name}</h3>
+      <h3>Recipe: {props.taco.condiment.recipe}</h3>
     </div>
   );
 };
